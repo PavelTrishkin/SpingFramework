30f5d354e740865da832091b49497980ad228929
Павел <p-trishkin1@mail.ru>
2020-09-14 01:36:49 +0300

    Lesson3

---
+++ lesson3/src/main/java/ru/gb/trishkin/lesson3/domain/Product.java
@@ -0,0 +1,64 @@
+package ru.gb.trishkin.lesson3.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private float price;
+
+    @OneToMany(mappedBy = "product")
+    private List<Order> orders;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product [id = %d, title = %s, price = %.2f, orders_count = %d]", id, title, price, orders.size());
+    }
+}
